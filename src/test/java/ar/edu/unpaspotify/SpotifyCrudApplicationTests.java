7c36d95ac00c9903ee9777d50934c261050ac91c
Nicolás Butterfield <nicobutter@gmail.com>
2025-06-03 18:24:12 -0300

    Primer commit

---
+++ src/test/java/ar/edu/unpaspotify/SpotifyCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unpaspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
